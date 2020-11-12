2199eb10fa24967928a539f6b46bdab398e96a5f
agnieszka <agnieszka.sierbista@gmail.com>
2020-11-12 21:04:26 +0100

    Bundle as library

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from "react";
-import ReactDOM from 'react-dom';
 
 import Component from "./Component.layout.jsx";
 
-
-ReactDOM.render(
-    <Component />,
-    document.getElementById('root')
-);
+export {
+    Component
+}
